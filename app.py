4d0d89d5c0d8585818a83af432ebce012439adf9
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-09-24 08:57:15 +0300

    Made global ENTITIES

--- app.py
+++ app.py
@@ -10,13 +10,12 @@ def get_entities():
         return json.load(f)
 
 
+ENTITIES = get_entities()
+
+
 @app.route('/')
 def index():
-    # with open('entities.json') as f:
-    #     entities = json.load(f)
-    #     return render_template("main-all-items.html", entities=entities)
-    entities = get_entities()
-    return render_template("main-all-items.html", entities=entities)
+    return render_template("main-all-items.html", entities=ENTITIES)
 
 
 @app.route('/paging')
@@ -27,12 +26,11 @@ def paging():
 @app.route('/search')
 def search():
     model = request.args.get('model')
-    entities = get_entities()
     response = []
     if not model:
-        response = entities
+        response = ENTITIES
     else:
-        for e in entities:
+        for e in ENTITIES:
             if e["model"] == model:
                 response.append(e)
     return render_template("search_ause.html", entities=response)
@@ -40,11 +38,9 @@ def search():
 
 @app.route('/card/<int:eid>')
 def card(eid: int):
-    with open('entities.json') as f:
-        entities = json.load(f)
-        for ent in entities:
-            if ent["id"] == eid:
-                return render_template("card_full.html", entity=ent)
+    for ent in ENTITIES:
+        if ent["id"] == eid:
+            return render_template("card_full.html", entity=ent)
 
 
 if __name__ == '__main__':
